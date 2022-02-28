b239adc89d42ecd5a997c07b80b609051fd33769
Maciej <mszakacz@gmail.com>
2022-02-28 20:25:46 +0100

    feat(videos_list): Create a VideosList Page and Bloc

---
+++ lib/videos_list/bloc/videos_list_event.dart
@@ -0,0 +1,4 @@
+part of 'videos_list_bloc.dart';
+
+@immutable
+abstract class VideosListEvent {}
---
+++ lib/videos_list/bloc/videos_list_state.dart
@@ -0,0 +1,6 @@
+part of 'videos_list_bloc.dart';
+
+@immutable
+abstract class VideosListState {}
+
+class VideosListInitial extends VideosListState {}
